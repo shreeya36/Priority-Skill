7272c32878af2ae94692252d2967a282846a4adc
Shreeya Patil <shreeyap362002@gmail.com>
2023-03-18 21:38:19 +0530

    New Componet Added

--- prioritySkillCenter/src/app/app-routing.module.ts
+++ prioritySkillCenter/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CourseComponent } from './course/course.component';
+import { DetailComponent } from './detail/detail.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "home",
+  component: HomeComponent
+}, {
+  path: "detail",
+  component: DetailComponent
+},
+{
+  path: "course",
+  component:CourseComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
